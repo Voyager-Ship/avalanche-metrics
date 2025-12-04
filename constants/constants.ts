cd6085f4aca0624f5fe424da574ebdb795235954
simon0820s <simon0820s@hotmail.com>
2025-12-03 22:16:33 -0500

    bugfix(testMode): wire constant to .env variable

--- constants/constants.ts
+++ constants/constants.ts
@@ -5,5 +5,5 @@ export const GITHUB_API_KEY = process.env.GITHUB_API_KEY
 export const GITHUB_APP_ID = process.env.GITHUB_APP_ID
 export const NEON_CONNECTION_STRING = process.env.NEON_CONNECTION_STRING
 export const AUTH_API_KEY = process.env.AUTH_API_KEY
-export const TEST_MODE = true
+export const TEST_MODE = process.env.TEST_MODE === 'true' 
 export const MAX_USERS_PER_REQUEST = 1000
